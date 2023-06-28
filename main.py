05c50c17f5fcac10c5fd52dbd3c612621f28665b
valentina <guryanovavv21@gmail.com>
2023-06-28 23:35:19 +0300

    new main.py

--- main.py
+++ main.py
@@ -3,6 +3,9 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+from func.py import add_numbers
+
+add_numbers()
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
